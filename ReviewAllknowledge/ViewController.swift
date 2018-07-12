c8732427f1dd960881aee938ca624de6a9f1f2b2
Brian La <Brian@brians-macbook-pro.local>
2018-07-12 08:32:31 +0700

    Initial Commit

---
+++ ReviewAllknowledge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReviewAllknowledge
+//
+//  Created by Brian La on 7/12/18.
+//  Copyright © 2018 BrianLa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
